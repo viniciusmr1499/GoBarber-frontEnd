71c39fa78279f05a8efae2fd08d90bc72496f075
Marcos Vinicius <viniciusmr1499@gmail.com>
2020-10-01 23:13:54 -0300

    aplicando animacao no toast

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -7,6 +7,7 @@ import { Container } from './styles';
 
 interface ToastProps {
     message: ToastMessage;
+    style: object;
 }
 
 const icons = {
@@ -15,7 +16,7 @@ const icons = {
     success: <FiCheckCircle size={24}/>
 }
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
     const { removeToast } = useToast();
 
     useEffect(() => {
@@ -29,7 +30,7 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
     }, [removeToast, message.id]);
 
     return(
-        <Container type={message.type} hasDecription={!!!message.description}>
+        <Container type={message.type} style={style} hasDecription={!!!message.description}>
             {icons[message.type || 'info']}
 
             <div>
